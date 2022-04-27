4cea4bbe6dc0efd707d67b397d84d64b03c2a793
Andrew Tsukuda <andrewtsukuda@gmail.com>
2022-04-26 18:53:14 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Cookies from 'js-cookie';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<AppComponent />);
 }
+
+class AppComponent extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { 
+      isAuthorizedUser: null
+    }
+  }
+
+  async componentDidMount() {
+    await this.check_login().then(isAuthorizedUser => {
+      this.setState({ isAuthorizedUser: isAuthorizedUser });
+      console.log("Should be setting isAuthorizedUser")
+      console.log(isAuthorizedUser)
+    })
+    
+  }
+
+  render() {
+    console.log("is rendering")
+    if (this.state.isAuthorizedUser) {
+      console.log("Logged in boi");
+      return (
+        <div className="App">
+          <p>Congrats! You're logged in</p>
+      </div>
+      )
+    } else if (this.state.isAuthorizedUser === null) {
+      console.log("it null, do nutin");
+    } else {
+      console.log("navigate to login")
+      this.navigateToLogin()
+    }
+    
+  }
+
+  navigateToLogin() {
+    // Navigate to Login
+    window.location.assign('/login');
+  }
+
+  isLoggedIn() {
+    return this.check_login()
+  }
+
+  async check_login() {
+    const url = 'http://127.0.0.1:8000/api/is_user_logged_in'
+    const axios = require('axios').default;
+    const config = {
+      headers:{
+        'Authorization': `Bearer ${Cookies.get('accessToken')}`
+      }
+    };
+    return await axios.get(url, config)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        return true;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        return false;
+      })
+  }
+}
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './Components/SignIn';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="login" element={<SignIn />} />
+        </Routes>
+    </BrowserRouter>,
   </React.StrictMode>
 );
 
